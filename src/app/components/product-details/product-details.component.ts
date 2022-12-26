e72f4388be870cb7191658b50f8c5a0a890008b2
Abbes larbaoui <larbaouiabbes@gmail.com>
2022-12-26 11:07:59 +0100

    add product details component

---
+++ src/app/components/product-details/product-details.component.ts
@@ -0,0 +1,34 @@
+import { ActivatedRoute } from '@angular/router';
+import { Product } from './../../common/product';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from 'src/app/services/product.service';
+
+@Component({
+  selector: 'app-product-details',
+  templateUrl: './product-details.component.html',
+  styleUrls: ['./product-details.component.css']
+})
+export class ProductDetailsComponent implements OnInit {
+
+  product: Product = new Product();
+
+  constructor(private productService: ProductService,
+              private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.route.paramMap.subscribe(() => {
+      this.hundleProductDetails();
+    })
+  }
+
+  hundleProductDetails() {
+    const theProductId: number = +this.route.snapshot.paramMap.get('id');
+
+    this.productService.getProduct(theProductId).subscribe(
+      data => {
+        this.product = data;
+      }
+    )
+  }
+
+}
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -15,6 +15,13 @@ export class ProductService {
 
   constructor(private httpClient: HttpClient) { }
 
+  getProduct(theProductId: number): Observable<Product> {
+
+    const productUrl = `${this.baseUrl}/${theProductId}`;
+
+    return this.httpClient.get<Product>(productUrl);
+  }
+
   getProductList(categoryId: number): Observable<Product[]> {
 
     const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${categoryId}`;
